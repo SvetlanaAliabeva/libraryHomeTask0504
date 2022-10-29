137165b3ed79dc37fe77f628db1ce097b5dc9d51
SvetlanaAliabeva <112935755+SvetlanaAliabeva@users.noreply.github.com>
2022-10-29 15:53:40 +0900

    Update code.py

--- task3/code.py
+++ task3/code.py
@@ -1,1 +1,21 @@
+# input_line = "one two one tho three"
+input_line = "She sells sea shells on the sea shore; The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells."
+# input_line = 'aba aba; aba @?"'
 
+def previous_index(line=input_line):
+    new_list = line.split()
+
+    result = [-1 for i in range(len(new_list))]
+
+    my_dict = {}
+
+    for i in range(len(new_list)):
+        if new_list[i] in my_dict:
+            result[i] = my_dict[new_list[i]]
+            my_dict[new_list[i]] = i
+        else:
+            my_dict[new_list[i]] = i
+
+    return print(*result)
+
+previous_index(input_line)
